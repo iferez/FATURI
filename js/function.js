d7439cde8292827512284690a778acf884ccd3f6
Ivan Ferez <ferezivan@hotmail.com>
2022-07-22 01:33:46 -0300

    Form dependencias - fix generales

--- js/function.js
+++ js/function.js
@@ -6,3 +6,46 @@ $(document).ready(function(){
 
 
 })
+
+
+function ocultoPantallaIngreso(){
+  let pantallaUno = document.querySelector('#ingresanteUno');
+  pantallaUno.style.display = 'none';
+
+  let pantallaDos = document.querySelector('#ingresanteDos')
+  pantallaDos.style.display = 'block';
+
+}
+
+function ocultoPantallaFormulario(){
+  let pantallaDos = document.querySelector('#ingresanteDos')
+  pantallaDos.style.display = 'none';
+
+  
+  let pantallaTres = document.querySelector('#ingresanteTres')
+  pantallaTres.style.display = 'block';
+
+  let btn = document.querySelector('#guardar')
+  btn.style.display = 'block';
+
+}
+
+
+function botonesEnFormulario(){
+  let observaciones = document.querySelector('#observaciones')
+
+  let btnGuardar = document.querySelector('#guardarbtn')
+  let btnSeguir = document.querySelector('#siguientebtn')
+
+ if(observaciones.length < 0){
+alert("holaaaa")
+  btnGuardar.style.display = 'block';
+  btnSeguir.style.display = 'none';
+
+ }
+
+
+}
+
+
+
