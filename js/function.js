9551f6c0ebae5e7b2732524968a6e899a803326c
Ivan Ferez <ferezivan@hotmail.com>
2022-09-22 16:38:39 -0300

    Reversionado para ver navegación de la maquetación

--- js/function.js
+++ js/function.js
@@ -3,29 +3,3 @@ $(document).ready(function () {
     $("nav .button span").toggleClass("rotate");
   });
 });
-
-function ocultoPantallaIngreso() {
-  let pantallaUno = document.querySelector("#ingresanteUno");
-  pantallaUno.style.display = "none";
-  let pantallaDos = document.querySelector("#ingresanteDos");
-  pantallaDos.style.display = "block";
-}
-
-function ocultoPantallaFormulario() {
-  let pantallaDos = document.querySelector("#ingresanteDos");
-  pantallaDos.style.display = "none";
-  let pantallaTres = document.querySelector("#ingresanteTres");
-  pantallaTres.style.display = "block";
-  let btn = document.querySelector("#guardar");
-  btn.style.display = "block";
-}
-
-function botonesEnFormulario() {
-  let observaciones = document.querySelector("#observaciones");
-  let btnGuardar = document.querySelector("#guardarbtn");
-  let btnSeguir = document.querySelector("#siguientebtn");
-  if (observaciones.length < 0) {
-    btnGuardar.style.display = "block";
-    btnSeguir.style.display = "none";
-  }
-}
